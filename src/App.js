9f9aa16efb698c3def25b6fc9029a2186d57e881
Gaurav Kumar Nayak <gauravnayak369@gmail.com>
2023-07-19 19:03:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import useLocalStorage from "use-local-storage";
+import "bootstrap/dist/css/bootstrap.css";
+import Nav from "react-bootstrap/Nav";
+import SqlHome from "./SqlHome";
+import Footer from "./Footer";
 
 function App() {
+  const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "dark" : "light"
+  );
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <div className="nav-container">
+        <div className="light-toggle">
+          <button onClick={switchTheme}>
+            {theme === "light" ? "🌙" : "☀️"}
+          </button>
+        </div>
+        <div className="main-content">
+          <Nav variant="tabs" defaultActiveKey="/home">
+            <Nav.Link eventKey="disabled" disabled>
+              Inactive
+            </Nav.Link>
+            <Nav.Item>
+              <Nav.Link href="/">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="disabled" disabled>
+                Inactive
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+        <div className="Github-container">
+          <a
+            style={{ textDecoration: "none" }}
+            href="https://github.com/Trikcode"
+          >
+            <button className="github"> Github</button>
+          </a>
+        </div>
+      </div>
+      <SqlHome />
+      <Footer />
     </div>
   );
 }
